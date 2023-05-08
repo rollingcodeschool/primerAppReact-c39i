77c5c2c35c37033cb7672fa544c36488cebc6c66
Emilse Arias <emilsearias@gmail.com>
2023-05-08 13:32:03 -0300

    instalamos react-boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Titulos from "./components/Titulos";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   //aqui vamos a escribir logica
@@ -7,10 +8,10 @@ function App() {
   return (
     //aqui va el maquetado y un poco de logica
     //<> es un Fragment
-    <>
+    <section className="container my-4">
      <Titulos></Titulos>
      <Titulos/>
-  </>
+  </section>
   );
 }
 
--- src/components/Titulos.jsx
+++ src/components/Titulos.jsx
@@ -4,12 +4,12 @@ const Titulos = () => {
   return (
     <>
       <div>
-        <h1>Prueba de titulo principal</h1>
+        <h1 className="display-4 text-center">Prueba de titulo principal</h1>
       </div>
-      <ul>
-        <li>item 1</li>
-        <li>item 2</li>
-        <li>item 3</li>
+      <ul className="list-group">
+        <li className="list-group-item">item 1</li>
+        <li className="list-group-item">item 2</li>
+        <li className="list-group-item">item 3</li>
       </ul>
     </>
   );
